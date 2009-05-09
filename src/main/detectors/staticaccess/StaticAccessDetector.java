dce29dd26e637648d2e7f2e15c9f48e69b3128ad
Adam Warski <adam@warski.org>
2009-05-09 13:10:24 +0200

    Initial detector of static fields access

--- src/main/detectors/staticaccess/StaticAccessDetector.java
+++ src/main/detectors/staticaccess/StaticAccessDetector.java
@@ -2,12 +2,15 @@ package detectors.staticaccess;
 
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.Detector;
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.SourceLineAnnotation;
+import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  */
-public class StaticAccessDetector implements Detector {
+public class StaticAccessDetector extends OpcodeStackDetector {
 	private final BugReporter bugReporter;
 
 	public StaticAccessDetector(BugReporter bugReporter) {
@@ -16,9 +19,23 @@ public class StaticAccessDetector implements Detector {
 
 	public void visitClassContext(ClassContext classContext) {
 		System.out.println("Visiting " + classContext.getXClass().getClassDescriptor().getClassName());
+		super.visitClassContext(classContext);
 	}
 
 	public void report() {
-		bugReporter.logError("StaticAccess error!");
+		super.report();
+	}
+
+	public void sawOpcode(int seen) {
+		System.out.println("seen = " + seen);
+		if (seen == GETSTATIC || seen == PUTSTATIC) {
+			System.out.println("REPORTING BUG");
+
+			BugInstance bugInstance = new BugInstance(this, "STATIC_ACCESS_NOT_ALLOWED", HIGH_PRIORITY)
+					.addClassAndMethod(this)
+					.addReferencedField(this)
+					.addSourceLine(this);
+			bugReporter.reportBug(bugInstance);
+		}
 	}
 }
