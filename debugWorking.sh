537d42a7536ecd9e92ca2ea8150241711147405a
Adam Warski <adam@warski.org>
2009-05-11 09:31:49 +0200

    Removing debug files

--- debugWorking.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ant
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5007 -jar ../findbugs-1.3.8/lib/findbugs.jar -textui -auxclasspath dist/staticaccess-detector-annotations.jar -pluginList dist/staticaccess-detector.jar test/working/**/Test1.class 
